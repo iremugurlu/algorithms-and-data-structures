73754dd1146a889e1893b835f317a0d410e22a30
Irem Ugurlu <iremugurlu8l@gmail.com>
2021-12-15 23:29:39 +0100

    add extras

--- data-structures/linked-list/LinkedList.java
+++ data-structures/linked-list/LinkedList.java
@@ -45,4 +45,18 @@ public class LinkedList {
         }
     }
 
+    public boolean hasCycle(Node head) {
+        if(head == null) return false;
+        Node fast = head.next;
+        Node slow = head;
+        while(fast != null && fast.next != null && slow != null) {
+            if(fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+
 }
