fa091255e5816f39ba45ae49a99666c096f1f36a
Irem Ugurlu <iremugurlu8l@gmail.com>
2021-12-18 18:58:51 +0100

    add replace words exercise

---
+++ exercises/tree/ReplaceWords.java
@@ -0,0 +1,19 @@
+public class ReplaceWords {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] words = sentence.split(" ");
+        String res = "";
+        for(String word : words) {
+            String replaced = replaceSuccessor(word, dictionary);
+            res += replaced + " ";
+        }
+        return res.substring(0, res.length()-1);
+    }
+    
+    public String replaceSuccessor(String word, List<String> dictionary) {
+        String res = word;
+        for(String d : dictionary) {
+            if(res.startsWith(d)) res = d;
+        }
+        return res;
+    }
+}
