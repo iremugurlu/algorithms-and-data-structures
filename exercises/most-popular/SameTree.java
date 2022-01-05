e08bd996b80a4532908e9f75634fdf99e41cf874
Irem Ugurlu <iremugurlu8l@gmail.com>
2022-01-05 09:44:16 +0100

    Update SameTree.java

--- exercises/most-popular/SameTree.java
+++ exercises/most-popular/SameTree.java
@@ -2,18 +2,16 @@
 // Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null) {
-            if(q == null) return true;
-            else return false;
-        } else {
-            if(q == null) return false;
-            else {
-                if(p.val != q.val) return false;
-                else {
-                    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-                }
-            }
+        if(p == null && q == null) {
+            return true;
+        } 
+        if(p == null || q == null){
+            return false;
         }
+        if(p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
     public class TreeNode {
